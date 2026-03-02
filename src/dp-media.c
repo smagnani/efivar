smagnani/efivar

commit 951ee6c1909d4ec5ef0212963299ec9fc561b453

dp-media: more efidp_node_size() fixups.
In a prior commit, Leo fixed one of _format_media_dn()'s invocations of
efidp_node_size() to handle the failure case.  This patch builds on
that, using our more robust math primitives in that case.  There's also
a second analogous issue:

 Error: OVERRUN (CWE-119):
 efivar-38/src/dp-media.c:133: return_constant: Function call "efidp_node_size(dp)" may return -1.
 efivar-38/src/dp-media.c:133: overrun-buffer-arg: Calling "format_hex_helper" with "(uint8_t *)dp + 4" and "(efidp_node_size(dp) - 4L) / 2L" is suspicious because of the very large index, 18446744073709551614. The index may be due to a negative parameter being interpreted as unsigned.
 #  131|           default:
 #  132|                   format(buf, size, off, "Media", "MediaPath(%d,", dp->subtype);
 #  133|->                 format_hex(buf, size, off, "Media", (uint8_t *)dp+4,
 #  134|                                   (efidp_node_size(dp)-4) / 2);
 #  135|                   format(buf,size,off, "Media",")");

This patch fixes that case as well.

Resolves: RHEL-27676
Signed-off-by: Peter Jones <pjones@redhat.com>

src/dp-media.c
@@ -58,14 +58,15 @@ _format_media_dn(unsigned char *buf, size_t size, const_efidp dp)
 		format_vendor(buf, size, off, "VenMedia", dp);
 		break;
 	case EFIDP_MEDIA_FILE: {
-		ssize_t node_size = efidp_node_size(dp);
-		if (node_size > 0) {
-			size_t limit = ((size_t) node_size
-					- offsetof(efidp_file, name)) / 2;
-			format_ucs2(buf, size, off, "File", dp->file.name, limit);
-		} else {
-			efi_error("Invalid node size");
+		ssize_t limit = efidp_node_size(dp);
+		size_t offset = offsetof(efidp_usb_wwid, serial_number);
+		if (limit < 0 ||
+		    SUB(limit,  offset, &limit) ||
+		    DIV(limit, 2, &limit)) {
+			efi_error("bad DP node size");
+			return -1;
 		}
+		format_ucs2(buf, size, off, "File", dp->file.name, limit);
 		break;
 			       }
 	case EFIDP_MEDIA_PROTOCOL:
@@ -131,12 +132,19 @@ _format_media_dn(unsigned char *buf, size_t size, const_efidp dp)
 		format(buf, size, off, "Ramdisk", ")");
 		break;
 					   }
-	default:
+	default: {
+		ssize_t limit = efidp_node_size(dp);
+		if (limit < 0 ||
+		    SUB(limit,  4, &limit) ||
+		    DIV(limit, 2, &limit)) {
+			efi_error("bad DP node size");
+			return -1;
+		}
 		format(buf, size, off, "Media", "MediaPath(%d,", dp->subtype);
-		format_hex(buf, size, off, "Media", (uint8_t *)dp+4,
-				(efidp_node_size(dp)-4) / 2);
+		format_hex(buf, size, off, "Media", (uint8_t *)dp+4, limit);
 		format(buf,size,off, "Media",")");
 		break;
+		 }
 	}
 	return off;
 }

commit 3bd0f70ae36d68b57615b1ed3815b603e91c8806

dp-media.c: Fix truncated print of media file names
    For example, the "EFI" extension is missing from this print:
      Boot0005*  ubuntu HD(1,GPT,46c59016-a916-4ad6-b5ea-303ae4b422aa,0x800,0x82000)/\EFI\UBUNTU\SHIMX64.

    This was introduced via a copy/paste error that caused calculated file name length to be too short
    by 3 wide characters.

    Fixes: 951ee6c
    Signed-off-by: Steven J. Magnani <steve.magnani@proton.me>

src/dp-media.c
@@ -59,7 +59,7 @@ _format_media_dn(unsigned char *buf, size_t size, const_efidp dp)
 		break;
 	case EFIDP_MEDIA_FILE: {
 		ssize_t limit = efidp_node_size(dp);
-		size_t offset = offsetof(efidp_usb_wwid, serial_number);
+		size_t offset = offsetof(efidp_file, name);
 		if (limit < 0 ||
 		    SUB(limit,  offset, &limit) ||
 		    DIV(limit, 2, &limit)) {