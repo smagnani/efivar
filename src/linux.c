smagnani/efivar

commit 4516f573f973fec46dbeaae6f8faf889315c072a

Rework network device path creation to generate full paths.
This seems to be needed for them to be considered valid at least for ASUS.

Signed-off-by: Steven J. Magnani <steve.magnani@proton.me>

src/linux.c
@@ -494,6 +494,90 @@ err:
 }
 
 struct device HIDDEN
+*network_device_get(const char * const ifname)
+{
+	struct device *dev;
+	char *linkbuf = NULL;
+	char *tmpbuf = NULL;
+	int rc;
+	ssize_t probe_stop_offset = 0;
+
+	dev = device_new();
+	if (!dev) {
+		return NULL;
+	}
+
+	dev->interface_type = network;
+	dev->ifname = strdup(ifname);
+	if (!dev->ifname)
+		goto err;
+
+	/*
+	 * find the device link, which looks like:
+	 * ../../devices/$PCI_STUFF/net/$IFACE
+	 */
+	rc = sysfs_readlink(&linkbuf, "class/net/%s", dev->ifname);
+	if (rc < 0 || !linkbuf)
+	        goto err;
+	dev->link = strdup(linkbuf);
+	if (!dev->link) {
+	        efi_error("strdup(\"%s\") failed", linkbuf);
+	        goto err;
+	}
+	debug("dev->link: %s", dev->link);
+
+	rc = sysfs_readlink(&tmpbuf, "class/net/%s/device", dev->ifname);
+	if (rc < 0 || !tmpbuf) {
+		debug("readlink of /sys/class/net/%s/device failed",
+		      dev->ifname);
+
+		dev->device = strdup("");
+	} else {
+		dev->device = strdup(tmpbuf);
+	}
+
+	if (!dev->device) {
+		efi_error("strdup(\"%s\") failed", tmpbuf);
+		goto err;
+	}
+
+	char *filepath = NULL;
+	rc = find_device_file(&filepath, "driver", "class/net/%s", dev->ifname);
+	if (rc >= 0) {
+		rc = sysfs_readlink(&tmpbuf, "%s", filepath);
+		if (rc < 0 || !tmpbuf) {
+			efi_error("readlink of /sys/%s failed", filepath);
+			goto err;
+		}
+
+		linkbuf = pathseg(tmpbuf, -1);
+		if (!linkbuf) {
+			efi_error("could not get segment -1 of \"%s\"", tmpbuf);
+			goto err;
+		}
+
+		dev->driver = strdup(linkbuf);
+	} else {
+		dev->driver = strdup("");
+	}
+
+	if (!dev->driver) {
+	        efi_error("strdup(\"%s\") failed", linkbuf);
+	        goto err;
+	}
+
+	probe_stop_offset = probe_link(dev, "net/");
+	if (probe_stop_offset < 0) {
+		goto err;
+	}
+
+	return dev;
+err:
+	device_free(dev);
+	return NULL;
+}
+
+struct device HIDDEN
 *partition_device_get(int fd, int partition)
 {
 	struct device *dev;
@@ -639,7 +723,7 @@ err:
 }
 
 int HIDDEN
-make_blockdev_path(uint8_t *buf, ssize_t size, struct device *dev)
+make_dev_path(uint8_t *buf, ssize_t size, struct device *dev)
 {
 	ssize_t off = 0;
 
@@ -675,22 +759,6 @@ make_mac_path(uint8_t *buf, ssize_t size, const char * const ifname)
 	int fd = -1, rc;
 	ssize_t ret = -1, sz, off = 0;
 	char busname[PATH_MAX+1] = "";
-	struct device dev;
-
-	memset(&dev, 0, sizeof (dev));
-	dev.interface_type = network;
-	dev.ifname = strdupa(ifname);
-	if (!dev.ifname)
-	        return -1;
-
-	/*
-	 * find the device link, which looks like:
-	 * ../../devices/$PCI_STUFF/net/$IFACE
-	 */
-	rc = sysfs_readlink(&dev.link, "class/net/%s", ifname);
-	if (rc < 0 || !dev.link)
-	        goto err;
-	debug("dev.link: %s", dev.link);
 
 	memset(&ifr, 0, sizeof (ifr));
 	strncpy(ifr.ifr_name, ifname, IF_NAMESIZE);
@@ -712,12 +780,7 @@ make_mac_path(uint8_t *buf, ssize_t size, const char * const ifname)
 	if (rc < 0)
 	        goto err;
 
-	sz = pci_parser.create(&dev, buf, size, off);
-	if (sz < 0)
-	        goto err;
-	off += sz;
-
-	sz = efidp_make_mac_addr(buf+off, size?size-off:0,
+	sz = efidp_make_mac_addr(buf, size,
 	                         ifr.ifr_ifru.ifru_hwaddr.sa_family,
 	                         (uint8_t *)ifr.ifr_ifru.ifru_hwaddr.sa_data,
 	                         sizeof(ifr.ifr_ifru.ifru_hwaddr.sa_data));

src/creator.c
@@ -249,7 +249,7 @@ efi_va_generate_file_device_path_from_esp(uint8_t *buf, ssize_t size,
 		 * symlink from /sys/dev/block/$major:$minor and get it
 		 * from there.
 		 */
-		sz = make_blockdev_path(buf, size, dev);
+		sz = make_dev_path(buf, size, dev);
 		if (sz < 0) {
 			efi_error("could not create device path");
 			goto err;
@@ -464,13 +464,27 @@ efi_generate_ipv4_device_path(uint8_t *buf, ssize_t size,
 			      uint16_t protocol,
 			      uint8_t addr_origin)
 {
+	struct device *dev = NULL;
 	ssize_t off = 0;
 	ssize_t sz;
 
-	sz = make_mac_path(buf, size, ifname);
+	dev = network_device_get(ifname);
+	if (dev == NULL) {
+		efi_error("could not get net interface info");
+		goto err;
+	}
+
+	sz = make_dev_path(buf, size, dev);
+	if (sz < 0) {
+		efi_error("could not create device path");
+		goto err;
+	}
+	off += sz;
+
+	sz = make_mac_path(buf+off, size ? (size - off) : 0, ifname);
 	if (sz < 0) {
 		efi_error("could not make MAC DP node");
-		return -1;
+		goto err;
 	}
 	off += sz;
 
@@ -479,18 +493,23 @@ efi_generate_ipv4_device_path(uint8_t *buf, ssize_t size,
 			    protocol, addr_origin);
 	if (sz < 0) {
 		efi_error("could not make IPV4 DP node");
-		return -1;
+		goto err;
 	}
 	off += sz;
 
 	sz = efidp_make_end_entire(buf+off, size?size-off:0);
 	if (sz < 0) {
 		efi_error("could not make EndEntire DP node");
-		return -1;
+		goto err;
 	}
 	off += sz;
 
+	device_free(dev);
 	return off;
+err:
+	device_free(dev);
+	return -1;
+
 }
 
 uint32_t PUBLIC

src/linux.h
@@ -148,14 +148,15 @@ struct device {
 };
 
 extern struct device HIDDEN *partition_device_get(int fd, int partition);
+extern struct device HIDDEN *network_device_get(const char * const ifname);
 extern void HIDDEN device_free(struct device *dev);
 extern int HIDDEN set_disk_and_part_name(struct device *dev);
 extern int HIDDEN set_part(struct device *dev, int value);
 extern int HIDDEN set_part_name(struct device *dev, const char * const fmt, ...);
 extern int HIDDEN set_disk_name(struct device *dev, const char * const fmt, ...);
 extern bool HIDDEN is_pata(struct device *dev);
-extern int HIDDEN make_blockdev_path(uint8_t *buf, ssize_t size,
-				     struct device *dev);
+extern int HIDDEN make_dev_path(uint8_t *buf, ssize_t size,
+				struct device *dev);
 extern int HIDDEN parse_acpi_hid_uid(struct device *dev, const char *fmt, ...);
 extern int HIDDEN eb_nvme_ns_id(int fd, uint32_t *ns_id);
 

commit 6b1845b1ee8770fa2c12ef9251974ea6107deafa

Fail IPv4 device path creation on network link parse error
An inability to parse a network device's PCI or ACPI path can lead to creation of an illegal boot entry that begins with the MAC component.

Instead, coax efibootmgr to print:
  Could not prepare Boot variable: Network unreachable

Fixes: 4516f57
Signed-off-by: Steven J. Magnani <steve.magnani@proton.me>

src/linux.c
@@ -571,6 +571,12 @@ struct device HIDDEN
 		goto err;
 	}
 
+	if (strncmp(dev->link + probe_stop_offset, "net/", 4) != 0) {
+	        efi_error("network device path parsing failed");
+	        errno = ENETUNREACH;
+	        goto err;
+	}
+
 	return dev;
 err:
 	device_free(dev);